324812c9030a0b8968ac45dd07224fea67a696f2
Berk Ezircan <berkezircan@gmail.com>
2021-02-05 23:39:13 +0300

    base layout

---
+++ src/components/Games.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Game from './Game';
+
+const Games = (props) => {
+  return <Game />;
+};
+
+Games.propTypes = {};
+
+export default Games;
